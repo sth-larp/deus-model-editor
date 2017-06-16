31a6512205e59959388336614449a260fc90a87c
Andy <andy@mechanus.ru>
2017-06-16 09:20:08 +0300

    тестовое использование API для отправки событий

--- src/app/model/deus-model.service.ts
+++ src/app/model/deus-model.service.ts
@@ -14,83 +14,89 @@ export class DeusModelService {
     private couchDbUrl = "http://dev.alice.digital:5984";
 
     private dbNames = {
-        base : "models-dev2",
-        work : "working-models-dev2",
-        view : "view-models-dev2"
+        base: "models-dev2",
+        work: "working-models-dev2",
+        view: "view-models-dev2"
     };
 
-    constructor(private http: Http)
-    {
+    constructor(private http: Http) {
         this.model = new DeusModel();
     }
 
 
     sentEvent(name: string, evtData: string): Observable<Response> {
-        //let url = "http://alice.digital:8157/events";
-        let url = this.couchDbUrl + "/events-dev2"
+        let url = "http://alice.digital:8157/events/" + this.charID;
+        //let h = new Headers([{ 'Content-Type': 'application/json' }, { 'Accept': 'application/json' }]);
+        //let url = this.couchDbUrl + "/events-dev2"
 
         let event = {
-                eventType : name,
-                timestamp: Date().valueOf(),
-                characterId: this.charID,
-                data : evtData
-            };
+            "events": [
+                {
+                    eventType: name,
+                    timestamp: Date.now().valueOf(),
+                    characterId: this.charID,
+                    data: evtData
+                }
+            ]
+        };
+
+        debugger;
 
         console.log("try to send!");
 
-        return this.http.post(url, event);
+        return this.http.post(url, JSON.stringify(event));
     }
 
     getModel(type: string): Observable<Response> {
-        if(!this.dbNames.hasOwnProperty(type)) { Observable.empty(); }
-        if(!this.charID){ return Observable.empty(); }
+        if (!this.dbNames.hasOwnProperty(type)) { Observable.empty(); }
+        if (!this.charID) { return Observable.empty(); }
 
         let url = this.couchDbUrl + "/" + this.dbNames[type] + "/" + this.charID;
 
-        let h = new Headers( [{ 'Content-Type' : 'application/json'}, {'Accept':'application/json'}] );
+        let h = new Headers([{ 'Content-Type': 'application/json' }, { 'Accept': 'application/json' }]);
         let options = new RequestOptions({ headers: h });
 
         let lastRev = "";
 
-        return Observable.timer(0,10000)
-                    .flatMap( x => this.http.get(url) )
-                    .map( response => response.json() )
-                    .filter( (json,i) => {
-                                if(json._rev != lastRev){
-                                    lastRev = json._rev;
-                                    return true;
-                                }
-                                console.log("Model not changed!");
-                                return false;
-                            } )
-                    .map( json => this.processModelJson(json) )
-                    .share();
+        return Observable.timer(0, 10000)
+            .flatMap(x => this.http.get(url))
+            .map(response => response.json())
+            .filter((json, i) => {
+                if (json._rev != lastRev) {
+                    lastRev = json._rev;
+                    return true;
+                }
+                console.log("Model not changed!");
+                return false;
+            })
+            .map(json => this.processModelJson(json))
+            .share();
 
     }
 
     //Преобразует JSON модели в объект с информацией об обновлении (пока заглушки)
-    private processModelJson( obj : any ): any {
-        if(obj == null) { return null; }
+    private processModelJson(obj: any): any {
+        if (obj == null) { return null; }
 
         let retObj: any = null;
 
-        if(!Array.isArray(obj)){
+        if (!Array.isArray(obj)) {
             retObj = {};
             retObj["__status"] = "none";
 
             /*if(Math.random() > 0.5){
                 retObj["__status"] = "mod";
             }*/
-        }else{
+        } else {
             retObj = [];
         }
 
-        for(let p in obj){
-            if(obj[p] == null ){
+        for (let p in obj) {
+            if (obj[p] == null) {
                 retObj[p] = { __value: null, __status: "none" }
-            }else if(typeof obj[p] == "object"){
-                retObj[p] = this.processModelJson( obj[p] );
-            }else if(typeof obj[p] == "string" || typeof obj[p] == "number" ||  typeof obj[p] == "boolean"){
+            } else if (typeof obj[p] == "object") {
+                retObj[p] = this.processModelJson(obj[p]);
+            } else if (typeof obj[p] == "string" || typeof obj[p] == "number" || typeof obj[p] == "boolean") {
                 retObj[p] = { __value: obj[p], __status: "none" }
 
                 //TODO test!!!
